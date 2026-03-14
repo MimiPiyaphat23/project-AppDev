1511990df76617e2f19c1b9393942ba9371a5a09
nampratai <nampratai.s@ku.th>
2026-03-14 18:45:19 +0700

    fix: update database connection password in get_connection function

--- project/backend/db.py
+++ project/backend/db.py
@@ -5,7 +5,7 @@ def get_connection():
         conn = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="",
+            password="123456",
             database="mallmap"
         )
         if conn.is_connected():
